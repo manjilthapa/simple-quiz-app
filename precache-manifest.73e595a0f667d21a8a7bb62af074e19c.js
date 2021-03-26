3b70fbfa8330af447ac66ff6f0890d0a212a369e
manjilthapa <manjilthapa@gmail.com>
2021-03-26 12:02:43 +0100

    Updates

---
+++ precache-manifest.73e595a0f667d21a8a7bb62af074e19c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f05898b473caf77c9d9904573d1d62e",
+    "url": "/simple-quiz-app/index.html"
+  },
+  {
+    "revision": "e6c986791b5ed2fbdb2b",
+    "url": "/simple-quiz-app/static/js/2.e20addf2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-quiz-app/static/js/2.e20addf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812ad86638846b32b181",
+    "url": "/simple-quiz-app/static/js/main.94ca0461.chunk.js"
+  },
+  {
+    "revision": "fcac8979f29bff05c5d4",
+    "url": "/simple-quiz-app/static/js/runtime-main.a022e00f.js"
+  }
+]);
